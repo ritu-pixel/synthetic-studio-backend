20e6eade1ee62bdb6ba5b14a2edb702ca0bbad56
Ritu Rajput <75141887+ritu-pixel@users.noreply.github.com>
2025-06-07 21:46:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -146,4 +146,7 @@ def balance_data():
 @app.route('/')
 def home():
     return jsonify({"message": "Synthetic Studio Backend is running"}), 200
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 
